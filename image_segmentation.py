c3a80800f4362e0d0939a5a0432a6b797d8ab4a1
abl57 <abl77@case.edu>
2025-04-08 17:24:24 -0400

    Save segmented image to files

--- image_segmentation.py
+++ image_segmentation.py
@@ -66,18 +66,6 @@ if __name__=="__main__":
         
     clusters = np.array(clusters, dtype=object)
     
-    '''
-    img = mpimg.imread(f"data/real/{file}.png")
-    plt.imshow(img)
-    plt.axis("off")  
-    plt.show()
-    '''
-    
-    img = mpimg.imread(f"data/reduced/{file}.png")
-    plt.imshow(img)
-    plt.axis("off")  
-    plt.show()
-    
     img = cv2.imread(f"data/reduced/{file}.png")
     
     # Width needs to match what is specified in graph_conversion
@@ -87,7 +75,5 @@ if __name__=="__main__":
         r, c = get_coordinates(clusters[i], width)
         for j in range(len(r)):
             img[r[j], c[j]] = np.floor((i / clusters.shape[0] - 1) * np.array([255, 255, 255]))
-        
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.axis("off")
-    plt.show()
+
+    cv2.imwrite(f"data/segmented/{file}.png", img)
