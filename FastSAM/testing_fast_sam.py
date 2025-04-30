3d080ffe279f706a0f214824133f6086ebb3229d
Sciguy128 <152509988+Sciguy128@users.noreply.github.com>
2025-04-30 14:04:31 -0400

    FastSAM

---
+++ FastSAM/testing_fast_sam.py
@@ -0,0 +1,12 @@
+from fastsam import FastSAM, FastSAMPrompt
+
+model = FastSAM('FastSAM/weights/FastSAM-x.pt')
+IMAGE_PATH = 'FastSAM/images/1.S.W.M.png'
+DEVICE = 'cpu'
+everything_results = model(IMAGE_PATH, device=DEVICE, retina_masks=True, imgsz=1024, conf=0.4, iou=0.9,)
+prompt_process = FastSAMPrompt(IMAGE_PATH, everything_results, device=DEVICE)
+
+# everything prompt
+ann = prompt_process.everything_prompt()
+
+prompt_process.plot(annotations=ann,output_path='FastSAM/output/1.S.W.M.png',)
