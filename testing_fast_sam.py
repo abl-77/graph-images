6bdce4dac4f489058eabcee3e3c4a9352f12e7f6
Sciguy128 <152509988+Sciguy128@users.noreply.github.com>
2025-04-30 14:18:03 -0400

    Fixes to make it work outside the FastSAM directory

--- testing_fast_sam.py
+++ testing_fast_sam.py
@@ -1,7 +1,13 @@
-from fastsam import FastSAM, FastSAMPrompt
+import sys
+import os
+
+# Add the parent directory of FastSAM to the module search path
+sys.path.append(os.path.abspath('FastSAM'))
+
+from FastSAM.fastsam import FastSAM, FastSAMPrompt
 
 model = FastSAM('FastSAM/weights/FastSAM-x.pt')
-IMAGE_PATH = 'FastSAM/images/1.S.W.M.png'
+IMAGE_PATH = 'Synthetic faces/1.S.W.M.png'
 DEVICE = 'cpu'
 everything_results = model(IMAGE_PATH, device=DEVICE, retina_masks=True, imgsz=1024, conf=0.4, iou=0.9,)
 prompt_process = FastSAMPrompt(IMAGE_PATH, everything_results, device=DEVICE)
@@ -9,4 +15,4 @@ prompt_process = FastSAMPrompt(IMAGE_PATH, everything_results, device=DEVICE)
 # everything prompt
 ann = prompt_process.everything_prompt()
 
-prompt_process.plot(annotations=ann,output_path='FastSAM/output/1.S.W.M.png',)
+prompt_process.plot(annotations=ann,output_path='output/1.S.W.M.png',)
